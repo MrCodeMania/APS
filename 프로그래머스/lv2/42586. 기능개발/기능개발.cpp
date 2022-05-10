65aa1aac3af211b19e18afdb213005f5dda73318
MrCodeMania <milk1543@naver.com>
2022-05-10 16:57:22 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 3.73 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+
+    int* Days = new int[progresses.size()];
+
+    // 날짜 계산
+    for(int i = 0; i < progresses.size(); i++)
+    {
+        int Remain_P = 100 - progresses[i];
+        int Day = 0;
+        while(Remain_P > 0)
+        {
+            Remain_P -= speeds[i];
+            Day++;
+        }
+
+        Days[i] = Day;
+    }
+
+    int Max = Days[0];
+    int Count = 0;
+
+    // 순차적 answer에 담기
+    for(int i = 0; i < progresses.size(); i++)
+    {
+        if(Days[i] > Max)
+        {
+            answer.push_back(Count);
+            Max = Days[i];
+            Count = 1;
+        }
+        else
+        {
+            Count++;
+        }
+    }
+    answer.push_back(Count);
+
+
+    return answer;
+}
