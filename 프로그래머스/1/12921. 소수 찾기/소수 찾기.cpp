e714ffc225b0ddd1525fb80da6308fed80e1ec6e
MrCodeMania <milk1543@naver.com>
2025-11-13 22:29:08 +0900

    [level 1] Title: 소수 찾기, Time: 3.49 ms, Memory: 4.51 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+
+int solution(int n) {
+    
+    bool IsComposite[1000001];
+    int answer = 0;
+    
+    for (int i = 2; i <= n; i++)
+    {
+        if(!IsComposite[i])
+        {    
+            for(int j = 2 * i; j <= n; j+=i)
+                IsComposite[j] = true;
+            answer++;
+        }
+    }
+    
+    return answer;
+}
